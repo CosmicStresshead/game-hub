45864bf0a97afb960aa7f766fd653bc32dfefef9
Ben Smylie <bensmylie@gmail.com>
2024-06-10 16:12:37 +0100

    Optimize image retrieval

---
+++ src/services/image-url.ts
@@ -0,0 +1,11 @@
+const getCroppedImageUrl = (url: string) => {
+  
+  // find media/ in url
+  const token = 'media/';
+  const index = url.indexOf(token) + token.length;
+  // insert cropping parameters
+  const croppedURL = url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  return croppedURL;
+}
+
+export default getCroppedImageUrl;
