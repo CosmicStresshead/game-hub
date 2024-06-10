137f89490ce68e88f1285c34ddb804052a55f91a
Ben Smylie <bensmylie@gmail.com>
2024-06-10 13:41:01 +0100

    Create api-client & GameGrid component

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,45 @@
+// REACT IMPORTS
+import { useEffect, useState } from "react";
+
+// PROJECT IMPORTS
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+
+//INTERFACES
+interface IGameObject {
+  id: number;
+  name: string;
+}
+
+interface IGamesResponse {
+  count: number;
+  results: IGameObject[];
+}
+
+const GameGrid = () => {
+  // State Hooks
+  const [games, setGames] = useState<IGameObject[]>([]);
+  const [error, setError] = useState("");
+
+  // Effect Hook
+  useEffect(() => {
+    apiClient
+      .get<IGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  });
+
+  // Component Markup
+  return (
+    <>
+      {error && <Text color="red">{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "e6317496a3e94bee8f200d341c9900cb",
+  }
+})
