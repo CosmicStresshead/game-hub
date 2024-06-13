0aa735316e14c6728f10fab41394fba3b83fa6fc
Ben Smylie <bensmylie@gmail.com>
2024-06-13 23:57:56 +0100

    Add more platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -6,9 +6,11 @@ import {
   FaXbox,
   FaLinux,
   FaAndroid,
+  FaKeyboard,
 } from "react-icons/fa";
+import { GiGameConsole } from "react-icons/gi";
 import { MdPhoneIphone } from "react-icons/md";
-import { SiNintendo } from "react-icons/si";
+import { SiNintendo, SiAtari, SiApplearcade, SiSega } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { IPlatform } from "../hooks/usePlatforms";
@@ -31,18 +33,30 @@ const PlatformIconList = ({ platforms }: Props) => {
     nintendo: SiNintendo,
     ios: MdPhoneIphone,
     web: BsGlobe,
+    atari: SiAtari,
+    sega: SiSega,
+    "3do": GiGameConsole,
+    "neo-geo": SiApplearcade,
+    "commodore-amiga": FaKeyboard,
   };
 
   return (
     <HStack>
-      {platforms.map((platform) => (
+      {platforms.slice(0, 6).map((platform) => (
         <Icon
+          title={platform.name}
           boxSize={6}
           key={platform.id}
           as={iconMap[platform.slug]}
           color={"gray.500"}
+          onClick={() => console.log(platform.slug)}
         />
       ))}
+      {platforms.length > 6 && (
+        <Text marginLeft={2} color="gray.400" fontSize="lg" fontWeight="bold">
+          (+{platforms.length - 6})
+        </Text>
+      )}
     </HStack>
   );
 };
